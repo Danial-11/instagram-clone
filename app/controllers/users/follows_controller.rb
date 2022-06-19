5f223819ae5b62d16265104a5575f2ad1fd270a9
Danial Azeem <dazeem.bscs18seecs@seecs.edu.pk>
2022-06-20 01:49:34 +0500

    deploy the app

--- app/controllers/users/follows_controller.rb
+++ app/controllers/users/follows_controller.rb
@@ -1,30 +1,30 @@
 # frozen_string_literal: true
 
-# class users for follow
+# class for follows
 class Users::FollowsController < ApplicationController
   before_action :set_user
 
   def create
-    return unless current_user.follow(@user.id)
-
-    respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
+    if current_user.follow(@user.id)
+      respond_to do |format|
+        format.html { redirect_to @user }
+        format.js
+      end
     end
   end
-end
 
-def destroy
-  return unless current_user.unfollow(@user.id)
-
-  respond_to do |format|
-    format.html { redirect_to @user }
-    format.js { render action: :create }
+  def destroy
+    if current_user.unfollow(@user.id)
+      respond_to do |format|
+        format.html { redirect_to @user }
+        format.js { render action: :create }
+      end
+    end
   end
-end
 
   private
 
-def set_user
-  @user = User.find_by(id: params[:user_id])
+  def set_user
+    @user = User.find_by(id: params[:user_id])
+  end
 end
